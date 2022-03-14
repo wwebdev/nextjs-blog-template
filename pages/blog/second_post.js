a31b8faffb240846b14f250511449aa3fb668641
Vincenius <vincent.will@vaitrade.de>
2022-03-14 18:44:20 +0100

    improve markdown

--- pages/blog/second_post.js
+++ pages/blog/second_post.js
@@ -3,11 +3,11 @@ import PostLayout from '../../components/Layout/PostLayout'
 const markdown = `
 You can also write posts in *Markdown*. Just add the create a post that looks like this:
 
-\`\`\`
+~~~
 <PostLayout useMarkdown={true}>
   your markdown
 </PostLayout>
-\`\`\`
+~~~
 
 
 I included the library [react-markdown](https://www.npmjs.com/package/react-markdown) to achieve that. For more details check their documentation.
